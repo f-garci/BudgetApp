1f5e1b4adaf1b86b7afbbf590b35d28ccdd6d734
f-garci <55116213+f-garci@users.noreply.github.com>
2021-04-17 20:44:54 -0700

    Update App.js

--- App.js
+++ App.js
@@ -5,37 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { HomeScreenStack } from "./Navigation/MainNavigator";
 import { AuthProvider } from "./contexts/AuthContext";
 
-// if (firebase.apps.length === 0) {
-//     firebase.initializeApp(fbconfig);
-// }
-
-// const authStateChange = (callback) => {
-//     return firebase.auth().onAuthStateChanged((user) => {
-//         if (user) {
-//             callback({ loggedIn: true });
-//         } else {
-//             callback({ loggedIn: false });
-//         }
-//     });
-// };
-
 export default function App() {
-    // const [user, setUser] = useState({ loggedIn: false });
-
-    // useEffect(() => {
-    //     const unsubscribe = authStateChange(setUser);
-
-    //     return () => {
-    //         unsubscribe();
-    //     };
-    // }, []);
-
-    // if (!user.loggedIn) {
-    //     console.log("user is logged out");
-    // } else {
-    //     console.log("user is logged in");
-    // }
-
     return (
         <AuthProvider>
             <NavigationContainer>
