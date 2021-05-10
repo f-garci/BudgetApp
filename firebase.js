f4dcdc2c9e62a1dd2cfa066406e4002483336a2e
f-garci <55116213+f-garci@users.noreply.github.com>
2021-05-10 09:45:32 -0700

    got rid of config.js

--- firebase.js
+++ firebase.js
@@ -1,31 +1,23 @@
 const firebase = require("firebase/app");
 require("firebase/auth");
 require("firebase/firestore");
-// import {
-//     REACT_APP_FIREBASE_API_KEY,
-//     REACT_APP_FIREBASE_AUTH_DOMAIN,
-//     REACT_APP_FIREBASE_PROJECT_ID,
-//     REACT_APP_FIREBASE_STORAGE_BUCKET,
-//     REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//     REACT_APP_FIREBASE_APP_ID,
-// } from "@env";
-
-const config = require("./config");
+import {
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID,
+} from "@env";
 
 const app = firebase.initializeApp({
-    apiKey: config.apiKey,
-    authDomain: config.authDomain,
-    databaseURL: config.databaseURL,
-    projectId: config.projectId,
-    storageBucket: config.storageBucket,
-    messagingSenderId: config.messagingSenderId,
-    appId: config.appId,
-    // apiKey: REACT_APP_FIREBASE_API_KEY,
-    // authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-    // projectID: REACT_APP_FIREBASE_PROJECT_ID,
-    // storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-    // messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    // appId: REACT_APP_FIREBASE_APP_ID,
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: `https://${REACT_APP_FIREBASE_PROJECT_ID}.firebaseio.com`,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
 });
 
 const auth = app.auth();
