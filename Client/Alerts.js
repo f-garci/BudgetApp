428498eba8f5214a56dc887658e5fc24b69bf364
f-garci <55116213+f-garci@users.noreply.github.com>
2021-05-15 01:41:52 -0700

    Added profile settings

--- Client/Alerts.js
+++ Client/Alerts.js
@@ -1,15 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import {View, Text, StyleSheet, Button, SafeAreaView, FlatList} from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Button,
+    SafeAreaView,
+    FlatList,
+} from "react-native";
 
 import { useAuth } from "../contexts/AuthContext";
 import { database } from "../firebase";
 import Dropdown from "../Components/Dropdown";
 
-const Alerts = props => {
-
+const Alerts = (props) => {
     const { currentUser } = useAuth();
-    const[transactions, setTransactions] = useState([]);
-    const[budget, setBudget] = useState(1000.0);
+    const [transactions, setTransactions] = useState([]);
+    const [budget, setBudget] = useState(1000.0);
     const months = [
         "January",
         "February",
@@ -28,7 +34,6 @@ const Alerts = props => {
     // var amount = 1000.0;
 
     const obtainTransactions = () => {
-
         useEffect(() => {
             const unsubscribe = database.transactions
                 .where("userId", "==", currentUser.uid)
@@ -46,35 +51,64 @@ const Alerts = props => {
             return unsubscribe;
         }, []);
 
-        return(
+        return (
             <View>
-                <FlatList 
-                    data = {transactions}
-                    renderItem = {({item}) => {
+                <FlatList
+                    data={transactions}
+                    renderItem={({ item }) => {
                         // var amount = item.budget;
                         // var update = amount - item.transAm;
-                        if(!item.category){
+                        if (!item.category) {
                             var amount = item.budget;
-                        var update = amount - item.transAm;  
-                            return(
+                            var update = amount - item.transAm;
+                            return (
                                 <View>
-                                    <Text>You paid {item.transAm} on {months[
-                                    new Date(item.createdAt.toDate()).getMonth()
-                                    ]} {new Date(item.createdAt.toDate()).getDate()}.</Text>
-                                    <Text>Your remaining balance is {item.remainingBudget}</Text>
+                                    <Text>
+                                        You paid {item.transAm} on{" "}
+                                        {
+                                            months[
+                                                new Date(
+                                                    item.createdAt.toDate()
+                                                ).getMonth()
+                                            ]
+                                        }{" "}
+                                        {new Date(
+                                            item.createdAt.toDate()
+                                        ).getDate()}
+                                        .
+                                    </Text>
+                                    <Text>
+                                        Your remaining balance is{" "}
+                                        {item.remainingBudget}
+                                    </Text>
                                 </View>
-                            )
-                        }else{
+                            );
+                        } else {
                             var amount = item.budget;
-                        var update = amount - item.transAm;
-                            return(
+                            var update = amount - item.transAm;
+                            return (
                                 <View>
-                                    <Text>You paid {item.transAm} for {item.category} on {months[
-                                    new Date(item.createdAt.toDate()).getMonth()
-                                    ]} {new Date(item.createdAt.toDate()).getDate()}.</Text>
-                                    <Text>Your remaining balance is: {item.remainingBudget}</Text>
+                                    <Text>
+                                        You paid {item.transAm} for{" "}
+                                        {item.category} on{" "}
+                                        {
+                                            months[
+                                                new Date(
+                                                    item.createdAt.toDate()
+                                                ).getMonth()
+                                            ]
+                                        }{" "}
+                                        {new Date(
+                                            item.createdAt.toDate()
+                                        ).getDate()}
+                                        .
+                                    </Text>
+                                    <Text>
+                                        Your remaining balance is:{" "}
+                                        {item.remainingBudget}
+                                    </Text>
                                 </View>
-                            )
+                            );
                         }
                     }}
                 />
@@ -88,12 +122,8 @@ const Alerts = props => {
                 <Text>{obtainTransactions()}</Text>
             </View>
         </SafeAreaView>
-        
-    )
-
-}
-const styles = StyleSheet.create({
-
-});
+    );
+};
+const styles = StyleSheet.create({});
 
 export default Alerts;
